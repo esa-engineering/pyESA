5e1c25abc433213a221b693b21f476e5523357c5
Antonio Miano <an.miano@gmail.com>
2026-02-19 11:35:19 +0100

    BestPractices_Update

--- pyESA.tab/pyESA.panel/pyESA.pulldown/BestPractices.pushbutton/BestPractices_script.py
+++ pyESA.tab/pyESA.panel/pyESA.pulldown/BestPractices.pushbutton/BestPractices_script.py
@@ -4,4 +4,5 @@ __context__ = "zero-doc"
 
 import webbrowser
 
-webbrowser.open_new(r'file://10.10.2.157\AreaTecnica\BIM\01_STANDARD\01_DOCUMENTALI\01_ITO E LINEE GUIDA\XXXXEN_XX.ITOa_00GL01-06 - BIM Best Practices.pdf')
+# webbrowser.open_new(r'file://10.10.2.157\AreaTecnica\BIM\01_STANDARD\01_DOCUMENTALI\01_ITO E LINEE GUIDA\XXXXEN_XX.ITOa_00GL01-06 - BIM Best Practices.pdf')
+webbrowser.open_new(r'https://esaengineeringsrl.sharepoint.com/sites/BIMESA-Wiki')
